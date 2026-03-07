7ed95798fd41ff36825e8c357640488f332045ce
zhangnianqiang <15840334992@163.com>
2026-03-08 00:12:22 +0800

    Revert "Update index.tsx"
    This reverts commit 470813baa3e2190955aa5f0181fe94660fe80f50.

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { FooterCopyright } from './FooterCopyright';
 
-export const Layout = ({ children }: { children: React.ReactNode }) => (
+const Layout = ({ children }: { children: React.ReactNode }) => (
   <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
     <main style={{ flex: 1 }}>{children}</main>
     <FooterCopyright />
   </div>
-);
-
-export const Root = ({ children }: { children: React.ReactNode }) => (
-  <React.Fragment>{children}</React.Fragment>
 );
 
 export default Layout;
