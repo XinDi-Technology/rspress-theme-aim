b3400de489309dcd563ab6ea1b2f8a1569fb4581
zhangnianqiang <15840334992@163.com>
2026-03-08 00:12:49 +0800

    Revert "Update index.tsx"
    This reverts commit 50263a0069e47b1dfea751046bd9b3fc136cb52d.

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import { Layout as BasicLayout } from '@rspress/core/theme-original';
 import { FooterCopyright } from './FooterCopyright';
 
-const Layout = ({ children }: { children: React.ReactNode }) => (
-  <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-    <main style={{ flex: 1 }}>{children}</main>
-    <FooterCopyright />
-  </div>
+const Layout = () => (
+  <BasicLayout afterFooter={<FooterCopyright />} />
 );
 
 export { Layout };
+export * from '@rspress/core/theme-original';
